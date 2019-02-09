a153fc77c063fad886bb634abb522d216a81ff9d
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2019-02-09 14:11:47 -0800

    use farmware_tools

--- set_servo_angle.py
+++ set_servo_angle.py
@@ -2,29 +2,11 @@
 
 """Set servo angle."""
 
-import os
-import json
-import requests
+from farmware_tools import get_config_value, device
 
-def get_env(key, type_=int):
-    'Return the value of the namespaced Farmware input variable.'
-    return type_(os.environ['{}_{}'.format(farmware_name, key)])
+farmware_name = 'Set Servo Angle'
 
-def set_servo_angle(pin, angle):
-    'Wrap the data in a `set_servo_angle` Celery Script command to send it.'
-    return {
-        "kind": "set_servo_angle",
-        "args": {"pin_number": pin, "pin_value": angle}}
+pin_number = get_config_value(farmware_name, config_name='pin_number')
+servo_angle = get_config_value(farmware_name, config_name='servo_angle')
 
-def post(wrapped_data):
-    'Send the Celery Script command.'
-    headers = {
-        'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-        'content-type': "application/json"}
-    payload = json.dumps(wrapped_data)
-    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
-                  data=payload, headers=headers)
-
-if __name__ == "__main__":
-    farmware_name = 'set_servo_angle'
-    post(set_servo_angle(get_env('pin_number'), get_env('servo_angle')))
+device.set_servo_angle(pin_number, servo_angle)
